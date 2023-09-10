6bcccd9dc76230fb6c52322d6cdb12cc537fd715
Alex-Uvarov <alexander.uvarov001@gmail.com>
2023-09-10 10:59:34 +0300

    feat: wrote revrse-int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+  let result = ``;
+  let number = Math.abs(n).toString();
+  for (let i = number.length; i !== 0; i--) {
+    result = `${result}${number[i - 1]}`;
+  };
+  return Number(result)
+};
