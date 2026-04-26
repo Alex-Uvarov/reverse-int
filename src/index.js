d4c869625b95932a3d28f5059fef9de1e8a54f1e
Alexander Uvarov <alexander.uvarov001@gmail.com>
2026-04-27 00:19:51 +0300

    The task is completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  let result = ``;
+  const number = Math.abs(n).toString();
+  for (let i = number.length; i !== 0; i -= 1) {
+    result = `${result}${number[i - 1]}`;
+  }
+  return Number(result);
 };
